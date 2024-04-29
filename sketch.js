ab1a9314021994d1f875881f65e764e50f5bffb8
cai84485228 <150638552+cai84485228@users.noreply.github.com>
2024-04-29 10:28:25 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var captureGraphics
 var capture_width = 640
 var capture_heigh = 480
+var span = 10
 function setup() {
   createCanvas(windowWidth, windowHeight);
   capture = createCapture(VIDEO)
@@ -14,11 +15,16 @@ function setup() {
 
 function draw() {
   background(220);
+  noStroke()
   push()
   translate(width/2-capture_width/2, height/2-capture_height/2) //把原點移到(width/2-160, height/2-120)
   image(capture,0,0) 
-  for(var y=0;y<captureGraphics.height;y=y+10){
-    var pixel = captureGraphics.get(x,y)
-    rect(x,y,10)
+  for(var x=0;x<captureGraphics.width;x=x+10){
+    for(var y=0;y<captureGraphics.height;y=y+10){
+      var pixel = captureGraphics.get(x,y)
+      fill(pixel)
+      rect(x,y,span)
+    }
   }
+  
 }
