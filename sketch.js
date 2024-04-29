fe29d13d86efc5b9056398d21b0ee7acf9937d10
cai84485228 <150638552+cai84485228@users.noreply.github.com>
2024-04-29 10:25:45 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,24 @@
+var captureGraphics
+var capture_width = 640
+var capture_heigh = 480
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
   capture = createCapture(VIDEO)
-  capture.size(320,240);//設定顯示畫面大小
+  capture.size(capture_width,capture_heigh);//設定顯示畫面大小
+  captureGraphics = createGraphics(capture_width,480)
+  captureGraphics.translate(capture_width,0)
+  captureGraphics.scale(-1,1)
+  //captureGraphics.hide()
+
 }
 
 function draw() {
   background(220);
   push()
-  translate(width/2-160, height/2-120)
-  image(capture,0,0)
-  pop()
+  translate(width/2-capture_width/2, height/2-capture_height/2) //把原點移到(width/2-160, height/2-120)
+  image(capture,0,0) 
+  for(var y=0;y<captureGraphics.height;y=y+10){
+    var pixel = captureGraphics.get(x,y)
+    rect(x,y,10)
+  }
 }
